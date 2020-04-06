244d6851e95eac9ff0d71b7d3e02edc4b0979744
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-04-06 18:22:35 +0200

    user init

---
+++ user/models.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, shopAdmin=False):
+        if not email:
+            return ValueError('Email pls')
+        client = self.model(
+            name=name,
+            email=email,
+            shopAdmin=shopAdmin
+        )
+        client.set_password(password)
+        client.save(using=self._db)
+        return client
+
+
+class User(AbstractBaseUser):
+    email = models.CharField(primary_key=True, max_length=200, verbose_name='email')
+    name = models.CharField(max_length=200, verbose_name='Full name')
+
+    shopAdmin = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+
+    latitude = models.DecimalField(decimal_places=7, max_digits=11, null=True)
+    longitude = models.DecimalField(decimal_places=7, max_digits=11, null=True)
+
+    object = UserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', ]
+
+    def get_short_name(self):
+        return self.name.split(' ')[0]
+
+    def get_full_name(self):
+        return self.name
