cf714bba904e41b2a379581234106414652e0fe5
Alberto Lopez <alberto.lopez@jediupc.com>
2020-04-07 20:12:00 +0200

    First CSS Version

--- hackovid/settings.py
+++ hackovid/settings.py
@@ -130,5 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
+print("Static path:", os.path.join(BASE_DIR, "static"))
 REGISTRATION_CODE = os.environ.get('REGISTRATION_TOKEN', '')
