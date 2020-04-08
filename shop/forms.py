6813d813a8207bb47361746b55d489eb1e8a9681
margaritageleta <noticiasmundiales99@gmail.com>
2020-04-08 14:44:15 +0200

    Slider done. THE BEST SLIDER IN THE HACKATHON.

--- shop/forms.py
+++ shop/forms.py
@@ -4,10 +4,14 @@ from django.forms import ModelForm
 from shop import models
 from localflavor.es.forms import ESIdentityCardNumberField
 
+from shop.range import RangeSliderField
+
 
 class ShopForm(ModelForm):
     # TODO: Obtain latitude and longitude from the street in google maps.
     CIF = ESIdentityCardNumberField(only_nif=False, label='', widget=forms.TextInput(attrs={'placeholder': 'CIF'}))
+
+    meanTime= RangeSliderField(label="", minimum=0, maximum=60,  step=5, name="How many time does the user stay in your shop while shopping?" )
 
     class Meta:
         model = models.Shop
@@ -15,13 +19,11 @@ class ShopForm(ModelForm):
 
         labels = {
             'name': '',
-            'meanTime': 'How many time does the user stay in your shop while shopping?'
         }
 
         help_text = {
             'CIF': 'CIF',
             'name': 'Name',
-            'meanTime': 'How many time does the user stay in your shop while shopping?'
         }
 
         exclude = ['latitude', 'longitude']
@@ -29,6 +31,8 @@ class ShopForm(ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'placeholder': 'Shop name'}),
         }
+
+
 
     def is_add_shop(self):
         return True
