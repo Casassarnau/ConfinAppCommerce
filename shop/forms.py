7503d6af21ceeaa359ca46576eb950a97a14f847
Alberto <alberto.lopez@jediupc.com>
2020-04-08 17:34:33 +0200

    Added shop view.

--- shop/forms.py
+++ shop/forms.py
@@ -7,6 +7,37 @@ from localflavor.es.forms import ESIdentityCardNumberField
 from shop.range import RangeSliderField
 
 
+class ShopFormView(ModelForm):
+    # TODO: Obtain latitude and longitude from the street in google maps.
+    CIF = ESIdentityCardNumberField(only_nif=False, label='CIF', widget=forms.TextInput(attrs={'placeholder': 'CIF'}))
+
+    meanTime = RangeSliderField(label='Mean Time', minimum=0, maximum=60,  step=5, name="How many time does the user stay in your shop while shopping?" )
+
+    class Meta:
+        model = models.Shop
+        fields = ['CIF', 'name', 'meanTime', 'secondaryCategories', 'services']
+
+        labels = {
+            'name': '',
+        }
+
+        help_text = {
+            'CIF': 'CIF',
+            'name': 'Name',
+        }
+
+        exclude = ['latitude', 'longitude']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Shop name'}),
+        }
+
+
+    def is_add_shop(self):
+        return True
+
+
+
 class ShopForm(ModelForm):
     # TODO: Obtain latitude and longitude from the street in google maps.
     CIF = ESIdentityCardNumberField(only_nif=False, label='', widget=forms.TextInput(attrs={'placeholder': 'CIF'}))
