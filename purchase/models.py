11054d5c40a2dbf2763df4c16ce362eb3d00bd90
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-04-09 22:43:17 +0200

    FUNCIONAAAAAAAAAAAAAA

--- purchase/models.py
+++ purchase/models.py
@@ -8,3 +8,4 @@ class Purchase(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='purchase')
     shop = models.ForeignKey(to=Shop, on_delete=models.DO_NOTHING, related_name='purchase')
     dateTime = models.DateTimeField()
+    endTime = models.DateTimeField()
