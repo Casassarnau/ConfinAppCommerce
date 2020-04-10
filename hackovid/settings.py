286f210d76a9c14b48b2b7d9183edf0594b80e07
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-04-11 00:28:17 +0200

    URL WORKS FINAL

--- hackovid/settings.py
+++ hackovid/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-APP_DOMAIN = os.environ.get('DOMAIN', None)
+APP_DOMAIN = 'https://home.nuroxator.com'
 
 
 # Quick-start development settings - unsuitable for production
