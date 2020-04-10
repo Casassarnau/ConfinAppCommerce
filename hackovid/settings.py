382d1032ae54489440d7454f5addc32f87786931
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-04-10 19:15:45 +0200

    Language do not change

--- hackovid/settings.py
+++ hackovid/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'es-ca'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
