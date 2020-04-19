3b766176ccf665b6b78f201ad5400899fe4d4fb2
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-04-19 22:48:12 +0200

    fix

--- hackovid/settings.py
+++ hackovid/settings.py
@@ -160,7 +160,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + '/staticfiles'
+STATIC_ROOT = BASE_DIR + '/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
